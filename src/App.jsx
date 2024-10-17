66596526a40b9e73b7b6ae7ad00b8c6d4989cc85
Anderson Balduino <anderson.balduino@synchat.com.br>
2024-10-17 10:30:59 -0300

    adding environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,61 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from "aws-amplify";
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.COGNITO_USER_POOL_ID,
+      userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID,
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+        name: {
+          required: true,
+        },
+        phone_number: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
+
+export default function App() {
+  //defining a constant with REACT_APP_API_URL value
+  const { COGNITO_USER_POOL_ID, COGNITO_USER_POOL_CLIENT_ID, SYNCHAT_CONVERSATION_HUB } = process.env;
 
   return (
-    <>
+  <div>
+    <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h2>Hello {user?.username} - {COGNITO_USER_POOL_ID}</h2>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <h3>User Pool Id: {COGNITO_USER_POOL_ID}</h3>
+      <h3>User Pool Client Id: {COGNITO_USER_POOL_CLIENT_ID}</h3>
+      <h3>Synchat Conversation Hub: {SYNCHAT_CONVERSATION_HUB}</h3>
+    </div>
+  </div>
+  );
 }
-
-export default App
